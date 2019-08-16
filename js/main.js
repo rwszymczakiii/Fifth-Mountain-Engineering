02ffe9f43c99430a94fab1295e4a5d985e1cff42
Robert  Szymczak III <rszymczak@meg.montrose-env.com>
2019-08-16 17:01:47 -0400

    more work

--- js/main.js
+++ js/main.js
@@ -1,12 +1,3 @@
-//sticky menu background
-window.addEventListener('scroll', function() {
-    if (window.scrollY > 150) {
-        document.querySelector('#navbar').style.opacity = 0.9;
-    } else {
-        this.document.querySelector('navbar').style.opacity = 1;
-    }
-});
-
 //smooth scrolling
 $('#navbar a, .btn').on('click', function(event) {
     if (this.hash !== '') {
@@ -14,9 +5,9 @@ $('#navbar a, .btn').on('click', function(event) {
 
         const hash = this.hash;
         $('html, body').animate({
-                scrollTop: $(hash).offset().top - 60
+                scrollTop: $(hash).offset().top - 100
             },
-            800
+            1200
         );
     }
 });
